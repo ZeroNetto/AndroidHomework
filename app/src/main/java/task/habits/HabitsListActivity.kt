2e8e438d1b088439dc010b787f111e92c9b1a9aa
Ivan Plaksin <zero.netto.o@gmail.com>
2020-04-09 18:13:52 +0500

    [feature] Add side navigation bar

--- app/src/main/java/task/habits/HabitsListActivity.kt
+++ app/src/main/java/task/habits/HabitsListActivity.kt
@@ -2,19 +2,34 @@ package task.habits
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class HabitsListActivity : AppCompatActivity(),
     HabitsListFragment.IHabitsListListener,
-    HabitEditorFragment.IHabitEditorListener {
+    HabitEditorFragment.IHabitEditorListener,
+    NavigationView.OnNavigationItemSelectedListener{
     private var viewPagerFragment: ViewPagerFragment = ViewPagerFragment()
     private var habitEditorFragment: HabitEditorFragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val drawerToggle = ActionBarDrawerToggle(this,
+            navigationDrawerLayout,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close)
+        drawerToggle.isDrawerIndicatorEnabled = true
+        navigationDrawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+        navigationDrawer.setNavigationItemSelectedListener(this)
 
         if(savedInstanceState == null){
             supportFragmentManager
@@ -72,5 +87,35 @@ class HabitsListActivity : AppCompatActivity(),
             .beginTransaction()
             .show(viewPagerFragment)
             .commit()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.menu_item_appInfo ->{
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.view_pager_placeholder, app_info_fragment())
+                    .commit()
+                floatingButton_add_habit.visibility = View.GONE
+            }
+            R.id.menu_item_mainPage ->{
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.view_pager_placeholder, viewPagerFragment)
+                    .commit()
+                floatingButton_add_habit.visibility = View.VISIBLE
+            }
+            else -> return false
+        }
+
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (navigationDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            navigationDrawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
